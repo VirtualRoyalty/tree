0ff3ecd8853d1351dcdd0c8669c21944a872ed77
anton <ashtanyuk@gmail.com>
2020-05-16 18:23:34 +0300

    edit 3

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,7 +6,7 @@ BST<std::string> makeTree(char* filename);
 
 TEST(lab6,test1_1)
 {
-    std::string filename="war_peace.txt";
+    char* filename="war_peace.txt";
     BST<std::string> tree=makeTree(filename);
     int depth=tree.depth();
     EXPECT_EQ(depth,35);
