bf31f854ba56280d3cd1e2314549d5f802e748a0
anton <ashtanyuk@gmail.com>
2020-05-16 18:34:58 +0300

    edit 4

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,21 @@
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
+#include  <cstdlib>
 
 BST<std::string> makeTree(char* filename)
 {
     BST<std::string> tree;
     char buf[256];
     int i=0;
-    std::ifstream file(filename);
+    std::ifstream file;
+    file.open(filename);
+    if(file.fail())
+    {
+    	std::cout<<"File not found!"<<std::endl;
+    	exit(1);
+    }
+
     while(!file.eof())
     {
         int ch=file.get();
